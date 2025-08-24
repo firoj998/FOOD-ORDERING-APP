31cf1cc80d74deca1390a552df4d22f1326725f8
Mohd Firoj <firojqv@gmail.com>
2025-08-24 18:21:10 +0530

    testcases for Contact US Page , and describe  method for grouping of testcases

--- src/components/Contact.test.jsx
+++ src/components/Contact.test.jsx
@@ -1,9 +1,36 @@
 import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import Contact from "./Contact";
 
-test("Should load contact us component", () => {
-  render(<Contact />); 
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
+// Testcase to check loading of a component
+
+// describe is used to combine the similar test cases together
+describe("Contact Us Page Test Case", () => {
+  test("Should load contact us component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  // Testing for button
+  test("Should load button inside Contact component", () => {
+    render(<Contact />);
+    const button = screen.getByRole("button"); // Pass
+    // const button = screen.getByText("Random");  // Fail
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Should load input name inside Contact component", () => {
+    render(<Contact />);
+    const inputName = screen.getByPlaceholderText("name");
+    expect(inputName).toBeInTheDocument();
+  });
+
+  test("Should load 2 input boxes on the Contact component", () => {
+    render(<Contact />);
+    const inputBoxes = screen.getAllByRole("textbox");
+    // console.log(inputBoxes.length)
+    expect(inputBoxes.length).toBe(2);
+    //expect(inputBoxes.length).not.toBe(3);
+  });
 });
